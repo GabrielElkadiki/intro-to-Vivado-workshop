0ca5c1c4dbab1e291236cc44f689e555a504cb11
Gabriel Elkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-11-09 14:14:26 -0500

    Update top.v

--- src/hdl/top.v
+++ src/hdl/top.v
@@ -31,7 +31,7 @@ module top(
     wire CLK_2_HZ;      // This is an important wire, it comes from the clock divider.
     clock_divider clk_div(          // This is how modules are instantiated. 
         .CLK_5_MHZ  (CLK_5_MHZ),    // They look similar to function calls but notice the '.'
-        .reset      (!CPU_RESETN),  // This is known as using named port connections, 
+        .reset      (reset),        // This is known as using named port connections, 
         .CLK_2_HZ   (CLK_2_HZ)      // you name the port you want to connect to on the left after a '.' and in brackets '()' your inputs/output wires
     );                              // The other option is using ordered port connections, I don't recommend those..
     
